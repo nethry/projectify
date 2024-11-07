41a9cb524f399aab0f9ffe515d6271880e5c1eb7
JaySeeReal <lego.kubass@gmail.com>
2024-11-07 14:30:23 +0100

    new entities

---
+++ src/Domain/Entities/Board/Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Projectify.Domain.Entities.Card;
+
+namespace Projectify.Domain.Entities.Board;
+
+public class Board(
+    string name
+    )
+{
+    string Name { get; private set; } = name;
+
+    list<Card> cards { get; private set; }
+}
---
+++ src/Domain/Entities/Card/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Projectify.Domain.Abstractions;
+namespace Projectify.Domain.Entities.Card;
+
+public class Card(
+	string name,
+	DateTime startDate,
+	DateTime deadline,
+	string description,
+	) : Entity
+{
+	string Name { get; set; } = name;
+
+	string Description { get; private set; } = description;
+
+	DateTime StartDate { get; private set; } = DateTime.UtcNow;
+	
+	DateTime deadline { get; private set; }
+}
