4ee87c5c5784bb9d73bab18cdcca7b75266b2e00
JaySeeReal <lego.kubass@gmail.com>
2024-11-13 11:16:12 +0100

    Single project handling

--- src/Application/Projects/Queries/List/GetProjectsListQueryHandler.cs
+++ src/Application/Projects/Queries/List/GetProjectsListQueryHandler.cs
@@ -1,5 +1,7 @@
 using MediatR;
+using Microsoft.VisualBasic;
 using Projectify.Application.Abstractions.Persistence.Repositories;
+using Projectify.Domain.Entities.Project;
 
 namespace Projectify.Application.Projects.Queries.List;
 
@@ -14,5 +16,29 @@ internal sealed class GetProjectsListQueryHandler(IProjectRepository repository)
 
         return projects
             .Select(project => new ProjectsListItemModel(project.Id, project.Name, project.Status));
+    }
+
+    // public async Task<IEnumerable<ProjectsListItemModel>> HandleSingleProject(
+    //     Guid Id,
+    //     GetProjectsListQuery query,
+    //     CancellationToken cancellationToken)
+    // {
+    //     var projects = await repository.GetAllAsync();
+        
+    //     var singleProject = projects.Single(project => project.Id == Id);
+    //     var project =  new ProjectsListItemModel(singleProject.Id,singleProject.Name, singleProject.Status);
+    //     return (IEnumerable<ProjectsListItemModel>)project;
+    // }
+
+    public async Task<IEnumerable<ProjectsListItemModel>> HandleSingleProject(
+        GetProjectsListQuery query,
+        CancellationToken cancellationToken)
+    {
+        var projects = await repository.GetAllAsync();
+
+        var singleProject = projects.Single(project => project.Id == query.Id);
+
+            var project = new ProjectsListItemModel(singleProject.Id,singleProject.Name, singleProject.Status);
+            return (IEnumerable<ProjectsListItemModel>)project;
     }
 }
